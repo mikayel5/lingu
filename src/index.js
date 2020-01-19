94081a508e761c46f6f6cb4d73ea5f488bb58a02
Mikayel5 <mikayelnahapetyan@gmail.com>
2020-01-19 14:45:32 +0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import Routes from "routes";
+import TopBar from "components/topBar";
+
+const App = () => {
+   return (
+      <div className="App">
+         <Router>
+            <TopBar />
+            <Routes />
+         </Router>
+      </div>
+   );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
